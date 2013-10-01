6d5b1e96691f82be49d9ab2a37d4aaa5b745ad78
dipnlik <dipnlik@gmail.com>
2013-10-01 09:40:32 -0300

    rails new paper_trail_sandbox -BT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaperTrailSandbox::Application.config.secret_key_base = '06ef489ac3015576eaf640937fe77bc1b7e3c394b191e4018862a818951cbba2ee86303525dc048c62ce75d2870da5de40671ada0208bcb8650ea5e10f1da2af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PaperTrailSandbox::Application.config.session_store :cookie_store, key: '_paper_trail_sandbox_session'
